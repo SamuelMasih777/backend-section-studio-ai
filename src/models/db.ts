ab3d4dea96f4beeb379b5c4832cf26ba12f5d278
SamuelMasih777 <samuelmasih.sls777@gmail.com>
2026-03-13 08:55:57 +0530

    feat: Add Sequelize PostgreSQL database connection setup with SSL and default model options.

--- src/models/db.ts
+++ src/models/db.ts
@@ -4,6 +4,12 @@ import config from './config';
 const sequelize = new Sequelize(config.databaseUrl, {
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    },
     define: {
         freezeTableName: true, // Prevent Sequelize from pluralizing table names
         timestamps: true      // Enable timestamps by default
